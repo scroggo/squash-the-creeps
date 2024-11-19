3c6365f20096928236bdb017755815f7010a7071
Leon Scroggins III <scroggo@gmail.com>
2024-11-18 19:00:14 -0500

    Add the ability to pause
    Godot makes this super easy - barely an inconvenience! Set the
    UserInterface's process mode to ALWAYS ("3" in the saved scene) so it
    will be processed even while paused.
    
    Update the check for reload - I haven't found a case where unwrapping
    here caused a problem, but I added the check in trying to discover the
    problem, and it matches the usage down below.
    
    (The real problem was the call below. This is doubly fixed, since we
    return after reloading, and now we check the `Option`.)

--- rust/src/user_interface.rs
+++ rust/src/user_interface.rs
@@ -10,18 +10,21 @@ struct UserInterface {
 #[godot_api]
 impl IControl for UserInterface {
     fn init(base: Base<Control>) -> Self {
-        Self {
-            base,
-        }
+        Self { base }
     }
     fn unhandled_input(&mut self, event: Gd<InputEvent>) {
         if event.is_action_pressed("ui_accept") {
-            if self
-                .base()
-                .get_node_as::<ColorRect>("Retry")
-                .is_visible()
-            {
-                self.base().get_tree().unwrap().reload_current_scene();
+            if self.base().get_node_as::<ColorRect>("Retry").is_visible() {
+                if let Some(mut tree) = self.base().get_tree() {
+                    tree.reload_current_scene();
+                    return;
+                }
+            }
+        }
+        if event.is_action_pressed("pause") {
+            if let Some(mut tree) = self.base().get_tree() {
+                let paused = tree.is_paused();
+                tree.set_pause(!paused);
             }
         }
     }
